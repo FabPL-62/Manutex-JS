6bff4c5db7a389c497d511d6f6befd134a7937cd
Fabian-Pulgar <fabian.pulgarl.12@sansano.usm.cl>
2017-06-23 00:43:29 -0400

    Se carga el proyecto a github

---
+++ app.js
@@ -0,0 +1,48 @@
+// se obtienen todas las librerias para el funcionamiento de la aplicacion
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// se cargan los controladores
+var index = require('./routes/index');
+
+// se inicia la aplicacion
+var app = express();
+
+// se obtienen las vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// referencia al archivo de controladores
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
