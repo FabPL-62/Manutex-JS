95566fd08a3c804b0ab79b03280ec671b1075981
Fabian-Pulgar <fabian.pulgarl.12@sansano.usm.cl>
2017-06-23 01:08:34 -0400

    se agrega less-middleware al proyecto

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
 
 // se cargan los controladores
 var index = require('./routes/index');
@@ -22,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // referencia al archivo de controladores
