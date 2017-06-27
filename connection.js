4d596d046060c3ae9b358e456ac14b947bd36323
FabPL-62 <fabian.pulgarl.12@sansano.usm.cl>
2017-06-26 22:22:22 -0400

    Se arreglan tabulaciones

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 // parametros para conectarse a la base de datos
 var connect_params = {
-	host     : '127.0.0.1',
+  host     : '127.0.0.1',
   user     : 'root',
   password : '',
   database : 'bdmanutex'
@@ -12,43 +12,43 @@ var connect_params = {
 // para realizar una query
 module.exports.query = function(_input, _callback) {
 
-	// se obtiene la conexion
-	var connect = mysql.createConnection(connect_params);
+  // se obtiene la conexion
+  var connect = mysql.createConnection(connect_params);
 
-	// se ve que es _input
-	var _query = null, _params = null;
-	if (typeof _input === "string") _query = _input;
-	else if (typeof _input === "object") {
+  // se ve que es _input
+  var _query = null, _params = null;
+  if (typeof _input === "string") _query = _input;
+  else if (typeof _input === "object") {
 
-		if (typeof _input[0] !== "undefined") 
-			_query = _input[0];
+    if (typeof _input[0] !== "undefined") 
+      _query = _input[0];
 
-		if (typeof _input[1] === "object") 
-			_params = _input[1];
-	}
+    if (typeof _input[1] === "object") 
+      _params = _input[1];
+  }
 
-	if (_query !== null) {
+  if (_query !== null) {
 
-		// se reemplazan los parametros si existen
-		if (_params !== null) {
-			_query = _query.replace(/\:(\w+)/g, function (txt, key) {
-		    if (_params.hasOwnProperty(key)) {
-		      return _params[key];
-		    }
-		    return txt;
-		  });
-		}
+    // se reemplazan los parametros si existen
+    if (_params !== null) {
+      _query = _query.replace(/\:(\w+)/g, function (txt, key) {
+        if (_params.hasOwnProperty(key)) {
+          return _params[key];
+        }
+        return txt;
+      });
+    }
 
-		// se realiza la query
-		connect.connect();
-		var result = connect.query(_query, function(error, results, fields){
-			if (error)
-				_callback(error,null);
-			else
-				_callback(null,results);
-		});
-		connect.end();
-	} else {
-		_callback("Bad query format: "+JSON.stringify(_input),null);
-	}
+    // se realiza la query
+    connect.connect();
+    var result = connect.query(_query, function(error, results, fields){
+      if (error)
+        _callback(error,null);
+      else
+        _callback(null,results);
+    });
+    connect.end();
+  } else {
+    _callback("Bad query format: "+JSON.stringify(_input),null);
+  }
 };
